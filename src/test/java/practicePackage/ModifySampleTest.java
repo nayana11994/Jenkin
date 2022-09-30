5fecd5d44f6fa52e87ce49ca9129022d8b15bd74
admin <admin@DESKTOP-MSBUJAU>
2022-09-30 17:09:50 +0530

    Modified

---
+++ src/test/java/practicePackage/ModifySampleTest.java
@@ -0,0 +1,13 @@
+package practicePackage;
+
+import org.testng.annotations.Test;
+
+public class ModifySampleTest 
+{
+	@Test
+	public void modify()
+	{
+		System.out.println("Modified Successfully");
+	}
+
+}
